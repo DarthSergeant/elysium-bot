26b2b46f6edccfeb2ec542a7b477669d0b15c9cb
DarthSergeant <darthsergeant@gmail.com>
2017-06-26 04:54:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,15 @@
 import os
 import sys
 import json
-import random
-
+import random          
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
-
 from flask import Flask, request
+
+satania = ['https://i.imgur.com/a0c99Xy.jpg', 'http://livedoor.blogimg.jp/goldennews/imgs/9/b/9b99e006.png', 'https://i.imgur.com/CYrJCal.jpg',
+           'https://i.ytimg.com/vi/bnkhnX_UIuY/maxresdefault.jpg', 'https://i.ytimg.com/vi/FeMww0y-bI0/hqdefault.jpg', 
+           'https://68.media.tumblr.com/53be6ed0e25c44b62897653af23d70d3/tumblr_okywwrfVhq1r5kws5o8_540.jpg'
+          ]
 
 app = Flask(__name__)
 
@@ -18,11 +21,6 @@ def webhook():
   log('Recieved {}'.format(data))
 
 #############################################
-satania = ['https://i.imgur.com/a0c99Xy.jpg', 'http://livedoor.blogimg.jp/goldennews/imgs/9/b/9b99e006.png', 'https://i.imgur.com/CYrJCal.jpg',
-           'https://i.ytimg.com/vi/bnkhnX_UIuY/maxresdefault.jpg', 'https://i.ytimg.com/vi/FeMww0y-bI0/hqdefault.jpg', 
-           'https://68.media.tumblr.com/53be6ed0e25c44b62897653af23d70d3/tumblr_okywwrfVhq1r5kws5o8_540.jpg'
-          ]
-           
            
   if data['text'] == '!lasagna':
     num = random.randint(0,len(satania)-1)
