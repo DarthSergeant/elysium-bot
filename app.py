48e06460d4d2e8b485ce2e16dd4f880aae2f4dfd
DarthSergeant <darthsergeant@gmail.com>
2017-06-26 05:24:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -23,9 +23,13 @@ def webhook():
   log('Recieved {}'.format(data))
 
 #############################################
-  if "no" in data['text']:
-    msg = "no u"
-    send_message(msg)
+  if data['name'] != 'Satania Bot':
+    if "no" in data['text']:
+           msg = "no u"
+           send_message(msg)
+    if "911" in data['text']:
+           msg = '911'
+           send_message(msg)
   if data['text'] == '!lasagna':
     num = random.randint(0,len(satania)-1)
     msg = satania[num]
