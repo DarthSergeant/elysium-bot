f78a67d8826027ab2ef9393b3ad7a47bdbfb8ac9
DarthSergeant <darthsergeant@gmail.com>
2017-06-27 00:31:43 -0400

    Create cat_facts.py

---
+++ database/cat_facts.py
@@ -0,0 +1,23 @@
+catfacts = [
+  "Cats are the most popular pet in the United States: There are 88 million pet cats and 74 million dogs.",
+  "There are cats who have survived falls from over 32 stories (320 meters) onto concrete.",
+  "A group of cats is called a clowder.",
+  "Cats have over 20 muscles that control their ears.",
+  "Your cat recognizes your voice but just acts too cool to care (probably because they are).",
+  'Owning a cat can reduce the risk of stroke and heart attack by a third.',
+  'In tigers and tabbies, the middle of the tongue is covered in backward-pointing spines, used for breaking off and gripping meat.',
+  'Cats sleep 70% of their lives.',
+  'A cat has been mayor of Talkeetna, Alaska, for 15 years. His name is Stubbs.',
+  'When cats grimace, they are usually "taste-scenting." They have an extra organ that, with some breathing control, allows the cats to taste-sense the air.',
+  'Evidence suggests domesticated cats have been around since 3600 B.C., 2,000 years before Egypts pharaohs.',
+  'A cats purr may be a form of self-healing, as it can be a sign of nervousness as well as contentment.',
+  'Adult cats only meow to communicate with humans.',
+  'The worlds richest cat is worth $13 million after his human passed away and left her fortune to him.',
+  'The oldest cat video on YouTube dates back to 1894 (when it was made, not when it was uploaded, duh).',
+  'In the 1960s, the CIA tried to turn a cat into a bonafide spy by implanting a microphone into her ear and a radio transmitter at the base of her skull. She somehow survived the surgery but got hit by a taxi on her first mission.',
+  'Female cats are typically right-pawed while male cats are typically left-pawed.',
+  'Cats make more than 100 different sounds whereas dogs make around 10.',
+  'Cats and humans have nearly identical sections of the brain that control emotion.',
+  'Cats have a longer-term memory than dogs, especially when they learn by actually doing rather than simply seeing.',
+  'Cats have 1,000 times more data storage than an iPad.'
+]
