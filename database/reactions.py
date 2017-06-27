6cec17294894a26644d56ccb92a4850415853b91
DarthSergeant <darthsergeant@gmail.com>
2017-06-27 15:07:02 -0400

    Create reactions.py

---
+++ database/reactions.py
@@ -0,0 +1,5 @@
+reaction = ['https://i.groupme.com/960x540.png.24b8dc9d17994b91b9803a8d4c67c750', #thank you
+            'https://i.groupme.com/362x294.png.cc3a1f4843d944e3be3a5596b9f7f263.large', #dog noises
+            'https://i.groupme.com/960x541.png.52921a87204a4c94ab0b489c2808902d.large', #I don't get it
+            'https://i.groupme.com/960x540.png.a5202fc4291a4ee6951ea7497bd80433' #stop me
+]
