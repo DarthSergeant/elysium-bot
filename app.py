77c6b3d4aac6f85363e0efdf56823065fc4fccac
DarthSergeant <darthsergeant@gmail.com>
2017-06-27 23:42:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
-#Note, this bot was created to respond to a friend in chat who repeats same pictures and phrases
-
 import os
 import sys
 import json
-import random
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 from flask import Flask, request
 
-from database.cat_facts import catfacts
-from database.lasagna import lasagna
-from database.reactions import reaction
-
-negatives = ['cannot', 'not', 'knot', 'annoyed', 'annoy', 'annoying']
+from database.ai import create_response
 
 app = Flask(__name__)
 
@@ -20,60 +13,12 @@ app = Flask(__name__)
 def webhook():
   data = request.get_json()
   log('Recieved {}'.format(data))
-  sentence = data['text']
-
-#############################################
-  if data['name'] != 'Satania Bot':
-    if "no" in sentence.lower():
-           msg = "no u"
-           send_message(msg)
-    if "911" in data['text']:
-            msg = '911'
-            send_message(msg)
-    if "awoo" in sentence.lower():
-           msg = 'awoo'
-           send_message(msg)
-    if 'fite me' in sentence.lower():
-           msg = 'fite me'
-           send_message(msg)
-  if "league" in sentence.lower():
-            msg = 'No'
-            send_message(msg)
-  if "shut up" in sentence.lower():
-      msg = "pls no bulli"
-      send_message(msg)
-#Commands
-  if sentence == '!roll20':
-     num = ((random.randint(0,19))+1)
-     msg = num
-     send_message(msg)
-  if sentence == '!roll6':
-     num = ((random.randint(0,5))+1)
-     msg = num
-     send_message(msg)
-  if sentence == '!lasagna':
-    num = random.randint(0,(len(lasagna)-1))
-    msg = lasagna[num]
-    send_message(msg)
-  if sentence == '!catfacts':
-    num = random.randint(0, (len(catfacts)-1))
-    msg = catfacts[num]
-    send_message(msg)
-  if sentence == '!thanks':
-    msg = reaction[0]
-    send_message(msg)
-  if sentence == '!dog':
-    msg = reaction[1]
-    send_message(msg)
-  if sentence == '!confuse':
-    msg = reaction[2]
-    send_message(msg)
-  if sentence == '!stop':
-    msg = reaction[3]
-    send_message(msg)
-
-
-#########################################
+  parse = data['text']
+  sentence = parse.lower()
+  response = create_response(sentence)
+  if response:
+    if data['name'] != "Lunar Bot":
+      send_message(response)
   return "ok", 200
 
 def send_message(msg):
