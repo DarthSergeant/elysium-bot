a45f23b5b0bd7cd060014b13c73d3d40162f29b7
DarthSergeant <darthsergeant@gmail.com>
2017-06-28 01:22:42 -0400

    Update reactions.py

--- database/reactions.py
+++ database/reactions.py
@@ -1,5 +1,8 @@
 reaction = ['https://i.groupme.com/960x540.png.24b8dc9d17994b91b9803a8d4c67c750', #thank you
             'https://i.groupme.com/362x294.png.cc3a1f4843d944e3be3a5596b9f7f263.large', #dog noises
             'https://i.groupme.com/960x541.png.52921a87204a4c94ab0b489c2808902d.large', #I don't get it
-            'https://i.groupme.com/960x540.png.a5202fc4291a4ee6951ea7497bd80433' #stop me
+            'https://i.groupme.com/960x540.png.a5202fc4291a4ee6951ea7497bd80433', #stop me
+            'x', #obmqa
+            'https://i.groupme.com/665x574.png.4f7ac7abc5e44054be7dd84aa315fc27', #am I dumb?
+            'x' #whatever 9
 ]
