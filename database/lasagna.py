debd891ec90017cac82edd21ce96f7b7e4785dd0
DarthSergeant <darthsergeant@gmail.com>
2017-06-30 22:55:06 -0400

    Delete lasagna.py

--- database/lasagna.py
+++
@@ -1,7 +0,0 @@
-lasagna = ['https://i.imgur.com/a0c99Xy.jpg',
-           'https://i.imgur.com/CYrJCal.jpg', 
-           'https://i.imgur.com/dbNDYcx.jpg', 
-           'https://i.imgur.com/bhnECWl.jpg', 
-           'https://i.imgur.com/gUcWy4j.jpg',
-           'https://i.ytimg.com/vi/fjbxTE4bx4k/maxresdefault.jpg'
-]
