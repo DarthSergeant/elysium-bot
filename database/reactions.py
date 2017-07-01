5890228ce2c0d58cf7d6e0b87fbb8308bd1c93aa
DarthSergeant <darthsergeant@gmail.com>
2017-06-30 22:55:24 -0400

    Update reactions.py

--- database/reactions.py
+++ database/reactions.py
@@ -1,8 +1,12 @@
+#need to move this links to imgur
 reaction = ['https://i.groupme.com/960x540.png.24b8dc9d17994b91b9803a8d4c67c750', #thank you
             'https://i.groupme.com/362x294.png.cc3a1f4843d944e3be3a5596b9f7f263.large', #dog noises
             'https://i.groupme.com/960x541.png.52921a87204a4c94ab0b489c2808902d.large', #I don't get it
             'https://i.groupme.com/960x540.png.a5202fc4291a4ee6951ea7497bd80433', #stop me
-            'x', #obmqa
+            'https://i.groupme.com/499x281.gif.5700d59dbae24cfcbc19d266ecd6e90c', #obmqa
             'https://i.groupme.com/665x574.png.4f7ac7abc5e44054be7dd84aa315fc27', #am I dumb?
-            'x' #whatever 9
+            'https://i.groupme.com/1200x900.png.3cd4e91e6952477595dbc5be5e5c15d3', #whatever 9
+            'https://i.groupme.com/960x480.png.149ce2228d644fa592de8c34ef9ac4b2', #weeaboo
+            'https://i.groupme.com/236x255.png.f8f3b4fe6d0a49658c651fd50a0e3d94.large', #wrong
+            'https://i.groupme.com/539x605.jpeg.163149e1673e4310aa6dc25cded2f026' #shark
 ]
