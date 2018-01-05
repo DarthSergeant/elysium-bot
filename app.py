478840dee3c3adfc6ab5ae2f82746048a82488cd
DarthSergeant <darthsergeant@gmail.com>
2018-01-05 01:07:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def webhook():
   name = data['name']
   parse = data['text']
   sentence = parse.lower()
-  response = create_response(sentence)
+  response = create_response(sentence, name)
   if response:
     if data['name'] != "Satania Bot":
       send_message(response)
