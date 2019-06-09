1f0276a5d12460dd118aeaca8795d2e31f42aa3b
DarthSergeant <darthsergeant@gmail.com>
2019-06-09 03:12:48 -0400

    Update ai.py

--- database/ai.py
+++ database/ai.py
@@ -11,42 +11,65 @@ eight_ball = ['It is certain', ' It is decidedly so', 'Without a doubt', 'Yes de
 five_ws = ['who', 'what', 'when', 'where', 'why', 'how']
 bot_tags = ['@elysia', '@ely', 'ely', 'elysia', ',,']
 replacement = ['you', 'a', 'an', 'the']
+#dnd_stats = ['str', 'int', 'wis', 'cha', 'con']
 
-def questions(sentence, name):
-    ###Question Prep###
-    seperate_questionmark = sentence.replace('?', ' ?')
-    sentence_list = seperate_questionmark.split(" ") #makes each word a list item
-    if any(word in sentence_list for word in replacement):
-        if 'a' in sentence_list:
-            sentence_list.remove('a')
-        if 'an' in sentence_list:
-            sentence_list.remove('an')
-        if 'the' in sentence_list:
-            sentence_list.remove('the')
-        if 'you' in sentence_list:
-            sentence_list = [w.replace('you', 'me') for w in sentence_list]
-    if " or " in sentence:
-        divider_word = sentence_list.index('or')
-        choices = [sentence_list[divider_word-1], sentence_list[divider_word+1]]
+def process_sentence(sentence, name): #Used to create the refined variable used for processing
+    seperate_comma = sentence.replace(',,', ' ,,') #,, is most common tag so this seperates it if a space is not used
+    seperate_questionmark = seperate_comma.replace('?', ' ?') #? is another common identifier so this makes sure it doesn't get combined with the data
+    sentence_list = seperate_questionmark.split(" ") #Makes sentence into a list
+    remove_words = True
+    while remove_words:
+        if any(word in sentence_list for word in replacement):
+                if 'a' in sentence_list:
+                    sentence_list.remove('a')
+                if 'an' in sentence_list:
+                    sentence_list.remove('an')
+                if 'the' in sentence_list:
+                    sentence_list.remove('the')
+                if 'you' in sentence_list:
+                    sentence_list = [w.replace('you', 'me') for w in sentence_list]
+        else:
+            remove_words = False
+        return sentence_list
+    
+def questions(refined):
+    if "or" in refined:
+        divider_word = refined.index('or')
+        choices = [refined[divider_word-1], refined[divider_word+1]]
         selection = random.randint(0,1)
         msg = choices[selection]
         return msg
-    elif any(word in sentence for word in five_ws):
+    elif any(word in refined for word in five_ws):
         msg = "Your w words confused me."
         return msg
     else:
         num = random.randint(0, (len(eight_ball)-1))
         msg = eight_ball[num]
         return msg
-    
+
+def dice_roller(refined):
+    divider_word = refined.index('roll')
+    number = refined[divider_word+1]
+    if number.isdigit() == True:
+        msg = random.randint(0, int(number))
+        return msg
+    else:
+        msg = "I only work with numerals"
+        return msg
+
 def create_response(sentence, name):
     msg = {}
     if sentence == "test":
         msg = "All systems are go"
-        time.sleep(1)
-        return msg
+    if sentence == 'wooloo': #Temporarily removes need to use tags.  Bot will respond to everything relevant
+        bot_tags.append(' ')
+        msg = "Tags removed."
     if any(word in sentence for word in bot_tags):
+        refined = process_sentence(sentence, name)
         if "?" in sentence:
-            msg = questions(sentence, name)
-            time.sleep(1)
-            return msg
+            msg = questions(refined)
+        if "roll" in sentence:
+            msg = dice_roller(refined)
+    time.sleep(.75)
+    return msg
+            
